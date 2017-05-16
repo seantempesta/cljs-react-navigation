77c0d3486f06e1ef245c726cb4c97f864e8a30b2
Sean Tempesta <sean.tempesta@gmail.com>
2017-05-16 10:01:51 +0700

    version bump to fix android example

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljs-react-navigation "0.1.0"
+(defproject cljs-react-navigation "0.1.1"
   :description "CLJS Wrappers for react-navigation"
   :url "https://github.com/seantempesta/cljs-react-navigation"
   :license {:name "MIT"
