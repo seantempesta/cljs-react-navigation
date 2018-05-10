0b968f8abf2b4c113117c2567b975087522c3017
Александар Симић <asimic@gmail.com>
2018-05-10 09:23:27 +0100

    Version bump up

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljs-react-navigation "0.1.2"
+(defproject cljs-react-navigation "0.1.3-SNAPSHOT"
   :description "CLJS Wrappers for react-navigation"
   :url "https://github.com/seantempesta/cljs-react-navigation"
   :license {:name "MIT"
